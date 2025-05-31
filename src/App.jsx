d680934f620235ec2b01fb0f51dc466352819aa4
jakjak100 <alfaawetalfa@gmail.com>
2025-06-01 00:53:19 +0700

    First

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+import './App.css';
+
+import MyProjects from '../src/components/MyProjects';
+import AboutMe from '../src/components/AboutMe';
+
+import { LuGitCompare } from "react-icons/lu";
+import { BiJoystick } from "react-icons/bi";
+import { RiStickyNoteAddLine } from "react-icons/ri";
+
+function App() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
+  const [myProjects, setMyProjects] = useState('-500px');
+  const [aboutMe, setAboutMe] = useState('0')
+
+  return (
+    <div className="container">
+      <div className="nav">
+        <h2>
+          <LuGitCompare style={{ fontSize: '32px', marginRight: '10px' }} />
+          <p>Jakthi Dyo Eliandi Putra</p>
+        </h2>
+        <div className="h3">
+          <h3 onClick={() => setAboutMe('1')}>About me</h3>
+          <h3 onClick={() => setMyProjects('0')}>My Projects</h3>
+        </div>
+      </div>
+
+      <div className="spline">
+        <spline-viewer url="https://prod.spline.design/bGFywlYV8zlhOz1f/scene.splinecode"></spline-viewer>
+      </div>
+
+      <MyProjects myProjects={myProjects} setMyProjects={setMyProjects} />
+
+      <AboutMe aboutMe={aboutMe} setAboutMe={setAboutMe} />
+
+      <div className="hover">
+        {isHovered2 && <BiJoystick className='joystick' />}
+
+        {isHovered && <RiStickyNoteAddLine className='note' />}
+      </div>
+
+      <footer>
+        <a className='linkedin' href="https://www.linkedin.com/in/jakthi-eliandi/"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          linkedin.com
+        </a>
+        <a className='github' href="https://github.com/jakthi"
+          onMouseEnter={() => setIsHovered2(true)}
+          onMouseLeave={() => setIsHovered2(false)}
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/MyProjects.jsx
@@ -0,0 +1,45 @@
+import '../style/MyProjects.css'
+
+function MyProjects({ myProjects, setMyProjects }) {
+    return (<>
+        <div className="container-myprojects" style={{ right: myProjects }}>
+            <h1
+                onClick={() => {
+                    setMyProjects('-500px');
+                }}
+            >
+                My Projects</h1>
+
+            <ul className='list'>
+                <li><a href="https://you-tube-clone-five-sand.vercel.app/">YouTube clone</a></li>
+                <li><a href="https://3d-website-with-react.vercel.app/">3D Website with React</a></li>
+                <li><a href="https://rgba-tools.vercel.app/">RGBA Tools</a></li>
+                <li><a href="https://zebra-crossing.vercel.app/">Zebra crossing</a></li>
+                <li><a href="https://jakthi.github.io/WhatsApp-clone/">WhatsApp clone</a></li>
+                <li><a href="https://amazon-clone-topaz-beta.vercel.app/">Amazon clone</a></li>
+                <li><a href="https://tic-tac-toe-react-hazel-ten.vercel.app/">Tic Tac Toe React</a></li>
+                <li><a href="https://jakthi.github.io/Game-Suwit-Jawa/">Game Suwit Jawa</a></li>
+                <li><a href="https://jakthi.github.io/Gallery/">Gallery</a></li>
+                <li><a href="https://anime-app-henna-one.vercel.app/">Anime App</a></li>
+                <li><a href="https://jakthi.github.io/Authetication-page/">Authentication page</a></li>
+                <li><a href="https://jakthi.github.io/QR-Generate/">QR Generate</a></li>
+                <li><a href="https://jakthi.github.io/Dashboard-responsive/">Dashboard responsive</a></li>
+                <li><a href="https://multiple-control.vercel.app/">Multiple control</a></li>
+            </ul>
+        </div>
+
+        <div className="blank"
+            style={{
+                visibility: myProjects === '0' ? 'visible' : 'hidden',
+                opacity: myProjects === '0' ? '0.4' : '0',
+            }}
+            onClick={() => {
+                setMyProjects('-500px');
+            }}
+        >
+        </div>
+    </>
+    )
+}
+
+export default MyProjects
